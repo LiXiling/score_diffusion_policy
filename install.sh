982fa62d2d09a94ec6faa53c44c2899d8a68f802
Maximilian Li <maximilian.xiling.li@gmail.com>
2023-11-28 14:44:18 +0100

    run beso

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ conda activate $ENVNAME
 
 # Order of Operations is important!
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+conda install -c conda-forge torchsde -y
 conda install -c conda-forge dill wandb tqdm einops zarr pandas numba -y
 conda install shapely scikit-image maptlotlib -y
 conda install -c conda-forge diffusers gymnasium pygame pymunk -y
@@ -13,10 +14,7 @@ conda install -c av
 
 conda update -c conda-forge hydra-core
 
-
-conda install lightning scipy matplotlib -c conda-forge
-
 conda install black isort pre-commit -c conda-forge
 
-pip install lightning-bolts
+pip install torchsde torchdiffeq
 pip install -e .
