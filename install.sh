28df9747e7ff5b20a8898a4ad444d805a54bf9b8
Maximilian Li <maximilian.xiling.li@gmail.com>
2023-11-29 15:05:45 +0100

    update isntall script

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 ENVNAME="score-diff-policy"
 
-conda create --name $ENVNAME python=3.11
-eval "$(conda shell.bash hook)"
-conda activate $ENVNAME
+conda create --name $ENVNAME python=3.11 &&
+eval "$(conda shell.bash hook)" &&
+conda activate $ENVNAME &&
 
 # Order of Operations is important!
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
-conda install -c conda-forge torchsde -y
-conda install -c conda-forge dill wandb tqdm einops zarr pandas numba -y
-conda install shapely scikit-image maptlotlib -y
-conda install -c conda-forge diffusers gymnasium pygame pymunk -y
-conda install -c av
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y &&
+#conda install -c conda-forge torchsde -y &&
+conda install -c conda-forge dill wandb tqdm einops zarr pandas numba -y &&
+conda install -c conda-forge shapely scikit-image matplotlib opencv -y &&
+conda install -c conda-forge diffusers gymnasium pygame pymunk -y &&
+conda install -c conda-forge av -y &&
 
-conda update -c conda-forge hydra-core
+# Install Last - Somehow always overwritten
+conda install -c conda-forge hydra-core -y &&
 
-conda install black isort pre-commit -c conda-forge
+conda install black isort pre-commit -c conda-forge &&
 
-pip install torchsde torchdiffeq
+pip install torchsde torchdiffeq  opencv-python&&
 pip install -e .
